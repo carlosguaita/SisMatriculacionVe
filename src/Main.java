395782d5816041dca439081b66e95db80103fb82
carlos.guaita <carlos.guaita@udla.edu.ec>
2026-03-25 08:43:06 -0500

    Actualización 2

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,14 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Vehiculo v1 = new Vehiculo();
-        Vehiculo v2 = new Vehiculo();
-
-        v1.marca = "Chevrolet";
-        v1.modelo = "Captiva";
-        v1.placa = "PDU1234";
-        v1.tipoCombustible = 1;
-        v1.numRuedas = 4;
-        v1.cilindraje = 1500;
-        v1.torque = 100;
+        Vehiculo v1 = new Vehiculo("Chevrolet","Captiva","PDU1234",
+                4,1500,100,1);
+        Vehiculo v2 = new Vehiculo("Ford","EcoSport","PDU4321",
+                4,1500,100,2);
 
         double accV1 = v1.aceleracion();
         System.out.println("La aceleración de V1 es: " + accV1);
         v1.informacion();
-
-        v2.marca = "Ford";
-        v2.modelo = "EcoSport";
-        v2.placa = "PDU4321";
-        v2.tipoCombustible = 2;
-        v2.numRuedas = 4;
-        v2.cilindraje = 1500;
-        v2.torque = 100;
 
         double accV2 = v2.aceleracion();
         System.out.println("La aceleración de V2 es: " + accV2);
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,12 +1,23 @@
 public class Vehiculo {
 
-    String marca;
-    String modelo;
-    String placa;
-    int numRuedas;
-    double cilindraje;
-    double torque;
-    int tipoCombustible; //1.Gasolina, 2.Diesel
+    private String marca;
+    private String modelo;
+    private String placa;
+    private int numRuedas;
+    private double cilindraje;
+    private double torque;
+    private int tipoCombustible; //1.Gasolina, 2.Diesel
+
+    public Vehiculo(String marca, String modelo, String placa,
+                    int numRuedas, double cilindraje, double torque, int tipoCombustible){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.numRuedas = numRuedas;
+        this.cilindraje = cilindraje;
+        this.torque = torque;
+        this.tipoCombustible = tipoCombustible;
+    }
 
     double aceleracion(){
         double acc;
